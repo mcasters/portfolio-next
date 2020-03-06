fd73d38234ff05b954c56e565ddb479c06ae3529
Unknown <yonk@wanadoo.fr>
2020-03-06 20:28:51 +0100

    Server side check authentication

--- src/data/graphql/services/authService.js
+++ src/data/graphql/services/authService.js
@@ -1,13 +1,18 @@
-import { User } from '../../models';
+import cookie from 'cookie';
+import jwt from 'jsonwebtoken';
+
+import {User} from '../../models';
+import config from '../../../../next.config';
+
+const JWT_SECRET = config.jwt.secret;
 
 const getAuthenticatedUser = async req => {
-  if (!req.session.userId) return false;
-
-  const user = await User.findOne({
-    where: { id: req.session.userId },
-  });
-
-  return !!user;
+  const { token } = cookie.parse(req.headers.cookie ?? '');
+  if (token) {
+    const { username } = jwt.verify(token, JWT_SECRET);
+    return User.findOne({where: {username}});
+  }
+  return false;
 };
 
 export default getAuthenticatedUser;
