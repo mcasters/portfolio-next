879e815e417cdebe49b3792e99ba53d11358051a
Unknown <yonk@wanadoo.fr>
2020-04-04 01:43:48 +0200

    LoginControl matches with ssr

--- src/components/LayoutComponents/Footer/LoginControl/LoginControl.js
+++ src/components/LayoutComponents/Footer/LoginControl/LoginControl.js
@@ -7,19 +7,14 @@ import ROUTER_CONSTANT from '../../../../constants/router';
 import getConfig from 'next/config';
 
 const LoginControl = () => {
+  const [isConnected, setIsConnected] = useState(false);
   const { publicRuntimeConfig } = getConfig();
   const { ls_key, ls_value } = publicRuntimeConfig;
-  const onLocalStorage =
-    typeof window !== 'undefined' ?
-    window.localStorage[ls_key] === ls_value : false;
-  const [isConnected, setIsConnected] = useState(onLocalStorage);
 
   useEffect(() => {
     if (typeof window !== 'undefined')
-      setIsConnected(
-        window.localStorage[ls_key] === ls_value,
-      );
-  }, [onLocalStorage]);
+      setIsConnected(window.localStorage[ls_key] === ls_value);
+  }, [isConnected]);
 
   if (isConnected) {
     return (
