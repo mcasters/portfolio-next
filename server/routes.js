352f128c5151b58bfdba941ac226cea9ac52141f
Unknown <yonk@wanadoo.fr>
2020-04-17 15:04:52 +0200

    Remove custom server

--- server/routes.js
+++
@@ -1,7 +0,0 @@
-// import Routes, * as nextRoutes from "next-routes";
-//
-// export const routes = nextRoutes() as Routes;
-// export const Router = routes.Router;
-// export const Link = routes.Link;
-//
-// routes.add("confirm", "/user/confirm/:token");
