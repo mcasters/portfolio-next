449c800ca9108cf9991faa90e85689efb7c58172
Unknown <yonk@wanadoo.fr>
2020-08-29 01:46:28 +0200

    Api calls drawing page

--- src/pages/dessins.js
+++ src/pages/dessins.js
@@ -1,30 +1,26 @@
-import Item from '../components/item-dir/item/Item';
-import ItemConstant from '../constants/itemConstant';
-import Layout from '../components/layout-components/layout/Layout';
-import { getAllItems } from '../data/api/api';
+import useSWR from 'swr';
 
-const Dessins = ({ items }) => {
-  const title = 'Dessins';
-  const type = ItemConstant.DRAWING.TYPE;
+import Item from '../components/item-dir/item/Item';
+import CONST from '../constants/itemConstant';
+import Layout from '../components/layout-components/layout/Layout';
+import { ALL_ITEMS } from '../data/graphql/api/queries';
+import { allItemsRequest } from '../data/graphql/api/query-graphql';
+
+const Dessins = () => {
+  const type = CONST.DRAWING.TYPE;
+  const { data } = useSWR([ALL_ITEMS, type], allItemsRequest);
 
   return (
     <Layout>
       <section>
-        <h1 className="hidden">{title}</h1>
-        {items.map((drawing) => (
-          <Item key={drawing.title} item={drawing} type={type} />
-        ))}
+        <h1 className="hidden">{CONST.DRAWING.TITLE}</h1>
+        {data &&
+          data.allItems.map((drawing) => (
+            <Item key={drawing.title} item={drawing} type={type} />
+          ))}
       </section>
     </Layout>
   );
 };
-
-export async function getServerSideProps() {
-  const items = await getAllItems(ItemConstant.DRAWING.TYPE);
-
-  return {
-    props: { items },
-  };
-}
 
 export default Dessins;
