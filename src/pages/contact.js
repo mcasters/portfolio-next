85f78dfdc5d747a60e3554e8da216634ce8083c2
Unknown <yonk@wanadoo.fr>
2020-08-28 23:31:15 +0200

    api calls contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,36 +1,41 @@
+import useSWR from 'swr';
+
 import Content from '../components/content/Content';
 import CONT_CONST from '../constants/content';
 import TITLE from '../constants/pageTitle';
 import s from './styles/contact.module.css';
 import Layout from '../components/layout-components/layout/Layout';
-import {getContent} from "../data/api/api";
+import { CONTENT } from '../data/graphql/api/queries';
+import { contentRequest } from '../data/graphql/api/query-graphql';
 
-export default function contact({ address, phone, email }) {
-  const title = TITLE.CONTACT;
+export default function contact() {
+  const { data: address } = useSWR(
+    [CONTENT, CONT_CONST.KEY.CONTACT_ADDRESS],
+    contentRequest,
+  );
+  const { data: phone } = useSWR(
+    [CONTENT, CONT_CONST.KEY.CONTACT_PHONE],
+    contentRequest,
+  );
+  const { data: email } = useSWR(
+    [CONTENT, CONT_CONST.KEY.CONTACT_EMAIL],
+    contentRequest,
+  );
+
   return (
     <Layout>
       <address>
-        <h1 className="hidden">{title}</h1>
+        <h1 className="hidden">{TITLE.CONTACT}</h1>
         <div className={s.contactContent}>
-          <Content content={address} />
+          {address && <Content text={address.content.text} />}
         </div>
         <div className={s.contactContent}>
-          <Content content={phone} />
+          {phone && <Content text={phone.content.text} />}
         </div>
         <div className={s.contactContent}>
-          <a href={`mailto:${email.text}`}>{email.text}</a>
+          {email && <a href={`mailto:${email.content.text}`}>{email.content.text}</a>}
         </div>
       </address>
     </Layout>
   );
 }
-
-export async function getServerSideProps() {
-  const address = await getContent(CONT_CONST.KEY.CONTACT_ADDRESS);
-  const phone = await getContent(CONT_CONST.KEY.CONTACT_PHONE);
-  const email = await getContent(CONT_CONST.KEY.CONTACT_EMAIL);
-  return {
-    props: { address, phone, email },
-  };
-}
-
