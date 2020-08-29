9c669df758a94f4799260b07a1e04cd8d51d0b85
Unknown <yonk@wanadoo.fr>
2020-08-29 12:11:43 +0200

    Api calls sculpture page

--- src/pages/sculptures.js
+++ src/pages/sculptures.js
@@ -1,28 +1,24 @@
-import Item from '../components/item-dir/item/Item';
-import ItemConstant from '../constants/itemConstant';
-import Layout from '../components/layout-components/layout/Layout';
-import {getAllItems} from "../data/api/api";
+import useSWR from 'swr';
 
-const Sculptures = ({ items }) => {
-  const title = 'Sculptures';
-  const type = ItemConstant.SCULPTURE.TYPE;
+import Item from '../components/item-dir/item/Item';
+import CONST from '../constants/itemConstant';
+import Layout from '../components/layout-components/layout/Layout';
+import { ALL_ITEMS } from '../data/graphql/api/queries';
+import { allItemsRequest } from '../data/graphql/api/query-graphql';
+
+const Sculptures = () => {
+  const type = CONST.SCULPTURE.TYPE;
+  const { data } = useSWR([ALL_ITEMS, type], allItemsRequest);
 
   return (
     <Layout>
-      <h1 className="hidden">{title}</h1>
-      {items.map(sculpture => (
+      <h1 className="hidden">{CONST.SCULPTURE.TITLE}</h1>
+      {data &&
+        data.allItems.map((sculpture) => (
           <Item key={sculpture.title} item={sculpture} type={type} />
         ))}
     </Layout>
   );
-}
-
-export async function getServerSideProps() {
-  const items = await getAllItems(ItemConstant.SCULPTURE.TYPE);
-
-  return {
-    props: { items },
-  };
-}
+};
 
 export default Sculptures;
