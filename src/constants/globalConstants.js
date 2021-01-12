427126c98cb732b3670442ba3c65251fb4df98c9
Marion <yonk@orange.fr>
2021-01-12 11:15:43 +0100

    Update date and some styling

--- src/constants/globalConstants.js
+++ src/constants/globalConstants.js
@@ -1,6 +1,6 @@
 const GLOBAL_CONSTANTS = {
   SITE_TITLE: 'Marion Casters',
-  COPYRIGHT: '© 2020 Marion Casters',
+  COPYRIGHT: '© 2021 Marion Casters',
   NOT_FOUND: "La page demandée n'existe pas.",
   EMAIL: 'contact@marioncasters.fr',
 };
