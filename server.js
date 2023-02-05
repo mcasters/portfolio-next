36e9e8d259345d1f417ecc41b112d2e2ed89e96a
Marion <contact@marioncasters.fr>
2023-02-05 08:05:38 +0100

    Try to solve Passenger issue

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ const port = 3000;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
+if (typeof PhusionPassenger !== 'undefined') {
+  PhusionPassenger.configure({ autoInstall: false });
+}
 app.prepare().then(() => {
-  createServer(async (req, res) => {
+  const server = createServer(async (req, res) => {
     try {
       // Be sure to pass `true` as the second argument to `url.parse`.
       // This tells it to parse the query portion of the URL.
@@ -19,14 +22,77 @@ app.prepare().then(() => {
       const { pathname, query } = parsedUrl;
 
       await handle(req, res, parsedUrl);
-
     } catch (err) {
       console.error('Error occurred handling', req.url, err);
       res.statusCode = 500;
       res.end('internal server error');
     }
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
   });
+
+  if (typeof PhusionPassenger !== 'undefined') {
+    server.listen('passenger');
+  } else {
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+  }
 });
+
+/*
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
+
+
+
+
+  const express = require('express')
+const next = require('next')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const port = 3001
+const app = next({ dev: true })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const server = express()
+
+    server.use('/api', createProxyMiddleware({ target: 'http://localhost:3000', ws: true }))
+
+    server.all('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
+    })
+})
+
+*/
