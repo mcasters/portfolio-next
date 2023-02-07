3c0c09ec2bd717848d9bf2d72bd6999780230204
Marion <contact@marioncasters.fr>
2023-02-07 08:04:10 +0100

    Bug fix for database port

--- src/data/models/sequelize.js
+++ src/data/models/sequelize.js
@@ -1,18 +1,32 @@
 /*eslint-disable no-undef*/
 import Sequelize from 'sequelize';
 
+const dev = process.env.NODE_ENV !== 'production';
+
+const devParams = {
+  host: process.env.DATABASE_HOST,
+  dialect: 'mysql',
+  define: {
+    freezeTableName: true,
+    timestamps: false,
+  },
+  port: process.env.DATABASE_PORT1 || process.env.DATABASE_PORT2,
+};
+
+const prodParams = {
+  host: process.env.DATABASE_HOST,
+  dialect: 'mysql',
+  define: {
+    freezeTableName: true,
+    timestamps: false,
+  },
+};
+
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST || 'localhost',
-    dialect: 'mysql',
-    define: {
-      freezeTableName: true,
-      timestamps: false,
-    },
-  },
+  dev ? devParams : prodParams,
 );
 
 export default sequelize;
